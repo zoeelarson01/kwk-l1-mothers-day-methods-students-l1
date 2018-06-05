bc89d86ed0e7d10952ebddb789ca82d0d3da3027
zoeelarson01 <zoeelarson@gmail.com>
2018-06-05 19:20:37 +0000

    automatically backed up by learn

---
+++ holiday_greeting.rb
@@ -0,0 +1,2 @@
+def holiday_greeting (recipient, holiday, author)
+  puts "Happy #{holiday}, #{recipient}!"
--- mothers_day.rb
+++ mothers_day.rb
@@ -1,6 +1,10 @@
 ## Define your method, mothers_day, below. Go through the README and update your method as needed!
 
+def mothers_day(recipient="Mom")
+  puts "Happy Mother's Day, #{recipient}!"
+end
 
 
 ## Call your method below - use puts to see it in the terminal!
 
+mothers_day
